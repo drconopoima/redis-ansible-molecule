5e37724afedaa5093c70e53e6ed7d8125e634ef6
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2020-08-03 23:12:13 +0200

    install_python.sh OpenSUSE,Arch,Fedora,Debian,RedHat

--- molecule/default/files/install_python.sh
+++ molecule/default/files/install_python.sh
@@ -1,18 +1,43 @@
 #!/bin/sh
 
-command -v python3 >/dev/null 2>&1 && exit 0
-command -v python >/dev/null 2>&1 && exit 0
-
-DEB_PACKAGE_NAME='python';
+# Section constants
+APT_PACKAGE_NAME='python3';
 YUM_PACKAGE_NAME='python36';
-if [ -n "$(command -v yum)" ]; then
-  yum -y update
-  yum install -y ${YUM_PACKAGE_NAME};
-elif [ -n "$(command -v apt-get)" ]; then
-  apt-get -y update
-  apt-get install -y ${DEB_PACKAGE_NAME};
-else
-  >&2 echo "ERROR: Cannot install python on destination"
-  exit 1;
+DNF_PACKAGE_NAME='python3';
+PACMAN_PACKAGE_NAME='python3';
+ZYPPER_PACKAGE_NAME='python3';
+
+# Debian/Ubuntu
+if command -v apt &>/dev/null; then
+  apt update && \
+  apt install -y "${APT_PACKAGE_NAME}" && \
+  (ln -s -T /usr/bin/${APT_PACKAGE_NAME} /usr/bin/python || true ) && \
+  exit 0;
+fi
+# CentOS/RedHat
+if command -v yum &>/dev/null; then
+  yum install -y ${YUM_PACKAGE_NAME} && \
+  (ln -s -T "/usr/bin/${YUM_PACKAGE_NAME}" /usr/bin/python || true ) && \
+  exit 0;
+fi
+# Fedora
+if command -v dnf &>/dev/null; then
+  dnf install -y "${DNF_PACKAGE_NAME}" && \
+  (ln -s -T "/usr/bin/${DNF_PACKAGE_NAME}" /usr/bin/python || true ) && \
+  exit 0;
+fi
+# Arch Linux
+if command -v pacman &>/dev/null; then
+  pacman -Syy --noconfirm "${PACMAN_PACKAGE_NAME}" && \
+  (ln -s -T "/usr/bin/${PACMAN_PACKAGE_NAME}" /usr/bin/python || true ) && \
+  exit 0;
+fi
+# OpenSUSE
+if command -v zypper &>/dev/null; then
+  zypper install -y "${ZYPPER_PACKAGE_NAME}" && \
+  (ln -sf -T "/usr/bin/${ZYPPER_PACKAGE_NAME}" /usr/bin/python || true ) && \
+  exit 0;
 fi
 
+echo "ERROR: Cannot install python3 on destination"
+exit 1;
