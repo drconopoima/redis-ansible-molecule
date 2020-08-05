d8f8a7abe9eead8d85d80c792057a312f1d55411
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2020-08-05 19:57:49 +0200

    Posix-shell compat. modify undef. &> for 2>&1

--- molecule/default/files/install_python.sh
+++ molecule/default/files/install_python.sh
@@ -8,32 +8,32 @@ PACMAN_PACKAGE_NAME='python3';
 ZYPPER_PACKAGE_NAME='python3';
 
 # Debian/Ubuntu
-if command -v apt-get &>/dev/null; then
+if command -v apt-get >/dev/null 2>&1; then
   apt-get update && \
   apt-get install -y "${APT_PACKAGE_NAME}" && \
   (ln -s -T /usr/bin/${APT_PACKAGE_NAME} /usr/bin/python || true ) && \
   exit 0;
 fi
 # CentOS/RedHat
-if command -v yum &>/dev/null; then
+if command -v yum >/dev/null 2>&1; then
   yum install -y ${YUM_PACKAGE_NAME} && \
   (ln -s -T "/usr/bin/${YUM_PACKAGE_NAME}" /usr/bin/python || true ) && \
   exit 0;
 fi
 # Fedora
-if command -v dnf &>/dev/null; then
+if command -v dnf >/dev/null 2>&1; then
   dnf install -y "${DNF_PACKAGE_NAME}" && \
   (ln -s -T "/usr/bin/${DNF_PACKAGE_NAME}" /usr/bin/python || true ) && \
   exit 0;
 fi
 # Arch Linux
-if command -v pacman &>/dev/null; then
+if command -v pacman >/dev/null 2>&1; then
   pacman -Syy --noconfirm "${PACMAN_PACKAGE_NAME}" && \
   (ln -s -T "/usr/bin/${PACMAN_PACKAGE_NAME}" /usr/bin/python || true ) && \
   exit 0;
 fi
 # OpenSUSE
-if command -v zypper &>/dev/null; then
+if command -v zypper >/dev/null 2>&1; then
   zypper install -y "${ZYPPER_PACKAGE_NAME}" && \
   (ln -sf -T "/usr/bin/${ZYPPER_PACKAGE_NAME}" /usr/bin/python || true ) && \
   exit 0;
